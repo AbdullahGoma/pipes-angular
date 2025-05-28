06d3c19bee212eb212af254913c2f38803e72a51
AbdullahGomaa <Abdullah.Goma.010170@gmail.com>
2025-05-28 18:55:59 +0300

    Temperature custom pipe

---
+++ src/app/temperature.pipe.ts
@@ -0,0 +1,22 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+    name: 'temp',
+    standalone: true
+})
+
+export class TemperaturePipe implements PipeTransform {
+    transform(value: string | number) {
+        let val: number;
+        if (typeof value === 'string') {
+            val = parseFloat(value);
+        }
+        else {
+            val = value
+        }
+
+        const outputTemperature = val * (9 / 5) + 32;
+
+        return `${outputTemperature} °F`;
+    }
+}
